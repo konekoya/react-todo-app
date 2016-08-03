9544bb24c3136f58a835ff77acb2bd89f0ac4751
Konekoya <tank158r5155@yahoo.com.tw>
2016-08-03 14:52:33 +0800

    Add delete mechanism

--- TodoApp.js
+++ TodoApp.js
@@ -1,19 +1,26 @@
 var Todo = React.createClass({
   getInitialState: function() {
     return {
-      items: ['Google', 'Amazon', 'Dropbox', 'Apple'],
+      items: ['Pay mobile phone bill', 'Pick up Andrew from daycare at 5 PM ', 'Grocery shopping', '10 minutes workout'],
       todo: ''
     };
   },
   handleSubmit: function(e) {
     e.preventDefault();
-
     if (this.state.todo.length > 0) {
       this.setState({
         items: this.state.items.concat([this.state.todo]),
         todo: ''
       });
     } 
+  },
+  handleRemoveTodo: function(e) {
+    let result = this.state.items.filter(function(item) {
+        return item !== e.target.textContent;
+    });
+    this.setState({
+      items: result
+    })
   },
   onChange: function(e) {
     this.setState({
@@ -24,12 +31,12 @@ var Todo = React.createClass({
     return (
       <div className="todo-container">
         <div className="todo-heading-container">
-          <h2 className="todo-heading">Milton's Todos</h2>
+          <h2 className="todo-heading">Kay's Todo List</h2>
           <span className="todo-count">Todo counts: {this.state.items.length}</span>
         </div>
-        <TodoList items={this.state.items} />
+        <TodoList items={this.state.items} onRemoveTodo={this.handleRemoveTodo} />
         <form className="todo-form" onSubmit={this.handleSubmit}>
-          <input className="todo-user-input" onChange={this.onChange} value={this.state.todo} type="text"/>
+          <input className="todo-user-input" required onChange={this.onChange} value={this.state.todo} type="text"/>
           <button className="todo-add">Add Todo</button>
         </form>
       </div>
@@ -40,7 +47,7 @@ var Todo = React.createClass({
 var TodoList = React.createClass({
   render: function() {
     return (
-      <ul className="todo-list">
+      <ul className="todo-list" onClick={this.props.onRemoveTodo}>
         {
           this.props.items.map(function(item) {
             return (
@@ -53,4 +60,5 @@ var TodoList = React.createClass({
   }
 });
 
+// Render the main component and append it to DOM
 ReactDOM.render(<Todo />, document.getElementById('app'));
